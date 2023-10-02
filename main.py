347da129030e6c34e522deb873ed152951b8c2aa
Patrick <p.townsend.2019@alumnos.urjc.es>
2023-10-02 17:50:56 +0200

    backward substitution implemented

--- main.py
+++ main.py
@@ -1,25 +1,47 @@
 import numpy as np
-def suma(x,y):
-    z = x+y
-    return z
+
 def LUdcmp(A):
-    N = np.shape(a)[0]
+    U = A
+    L = np.eye(np.shape(A)[0])
+    N = np.shape(A)[0]
     for j in range(1,N+1):
         for i in range(j+1,N+1):
-            l = U[i+1,j+1] / U[j+1,j+1]
-            
+            L[i-1,j-1] = U[i-1,j-1] / U[j-1,j-1]
+            U[i-1,:] =   U[i-1,:] - L[i-1,j-1]*U[j-1,:]
     return L,U
 
 
-
+def BackSub(L, U, b):
+    N = np.shape(L)[0]
+    y = np.zeros((N,1))
+    x = np.zeros((N,1))
+    # Forward substitution
+    y[0] = b[0]
+    for i in range(1,N+1):
+        y[i-1] = b[i-1] 
+        for j in range(1,i):
+            y[i-1] = y[i-1] - y[j-1]*L[i-1,j-1]
+    # backward substitution
+    x[N-1] = y[N-1]
+    for i in range(N,0,-1):
+        x[i-1] = y[i-1]
+        for j in range(i+1,N+1):
+            x[i-1] = x[i-1] - x[j-1]*U[i-1,j-1]
+        x[i-1] = x[i-1] / U[i-1,i-1]
+    return x
 
 
 if __name__ == "__main__":
     
-    a = np.array([[1,2,3],[4,6,5],[7,8,7]])
-    b = np.array([1,2,3])
-    L, U = LUdcmp
+    A = np.array([[1,2,3],[4,6,5],[7,8,7]])
+    b = np.array([1,1,2])
+    L, U = LUdcmp(A)
+    x = BackSub(L,U,b)
 
-    c = suma(a,b)
-    print("c",c)
+    print("L",L)
+    print("\n")
+    print("U",U)
+    print("\n")
+    print("A:", np.dot(L,U))
+    print("x:",x)
     
