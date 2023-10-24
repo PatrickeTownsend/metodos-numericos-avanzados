4b78f608672a2f0812bbecf9368dd547c5f81b8e
PatrickeTownsend <p.townsend.2019@alumnos.urjc.es>
2023-10-24 17:11:59 +0200

    added chebyshev nodal distribution

--- Lagrange_interpolation.jl
+++ Lagrange_interpolation.jl
@@ -13,8 +13,22 @@ n = 1000 # Numero de puntos a graficar
 
 #---Arrays---#
 xᵢ = LinRange(x_init, x_end, N)
-xₗ  = LinRange(x_init, x_end, n)
+xₗ  = ChebyshevNode(n) #LinRange(x_init, x_end, n)
 yᵢ = func(xᵢ)
-Pᵢ = zeros(N)
+yₗ  = func(xₗ)
+Pᵢ = zeros(n)
+Iᵢ = zeros(n)
+ϵ = zeros(n)
+for i in eachindex(xₗ)
+    Pᵢ[i] += Lagrange(xₗ[i] ,xᵢ ,yᵢ)[1]
+    ϵ[i] += epsilon(xₗ[i], xᵢ)
+end
+Eₗ = @.abs(yₗ - Pᵢ) 
+#Plot_func(xₗ, yₗ, Pᵢ) #Grafica la funciones exacta e interpolada
+
+scatter(xₗ , [log.(Eₗ), log.(abs.(ϵ))], title="Error de interpolacion", xlabel="xₗ", ylabel="log(Error)", label=["|f(x) -F(x)|" "ϵ(x)"],grid=true)
+
+
+
 
 
--- interp_functions.jl
+++ interp_functions.jl
@@ -8,13 +8,29 @@ function Lagrange(x ,xᵢ ,yᵢ)
                 Li *= (x - xᵢ[j])/(xᵢ[i] - xᵢ[j])
             end
         end
-        poly = poly + Li*yᵢ
+        poly = poly + Li*yᵢ[i]
     end
-    return poly,Li
+    return poly
 end
 
 function epsilon(x,xᵢ)
     N = length(xᵢ)
     π = prod(x - tᵢ for tᵢ in xᵢ)/(factorial(big(N+1)))
     return π
+end
+
+function ChebyshevNode(n)
+    x_ch = zeros(n)
+    for i =1:n
+        x_ch[i] = cos((2*i-1)*π/(2*n))
+    end
+    return x_ch
+end
+
+function Plot_func(xₗ, yₗ, Pᵢ)
+    plot(xₗ , yₗ, label="f(x) exacta")
+    scatter!(xₗ , Pᵢ, label="F(x) numerica")
+    xlabel!("xₗ")
+    ylabel!("y")
+    title!("Interpolacion")
 end
