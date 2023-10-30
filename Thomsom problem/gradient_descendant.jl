6c6674930816aaa78cf188e648e989b607294e5f
Patrick <p.townsend.2019@alumnos.urjc.es>
2023-10-30 16:40:20 +0100

    Improved running time

--- Thomsom problem/gradient_descendant.jl
+++ Thomsom problem/gradient_descendant.jl
@@ -1,52 +1,53 @@
 using Plots
 using LinearAlgebra
 using Random
-
-
 include("Functions.jl")
+
+#---Set up---#
 N = 32 # Number of charges
 max_iter = 10000 # Max iteration 
-max_tol = 1e-10 # Convergence tolerance
-N_iter = 1
+max_tol = 1e-18 # Convergence tolerance
+N_iter = 1 #Iteration counter
+α = 0.01 #Learning rate
+
+#---Arrays---#
 r = zeros(N,3)
-U = zeros(max_iter)
-α = 0.01
-r[:,:] = Initialization(N)
+U = zeros(2)
+
+#---Initial Guess---#
+#r[:,:] = Initialization(N)
+r[:,:] = InitRandom(N)
 U[1] = PotentialEnergy(r,N)
-#r[:,:] = InitRandom(N)
-ϵ= zeros(N,3,max_iter)
-while N_iter < max_iter 
+
+@time while N_iter < max_iter 
     tot_grad = zeros(3)
     for i = 1:N
         r[i, :] = r[i,:] - α*Gradient(r,i,N)
         r[i, :] /= norm(r[i,:])
         tot_grad += Gradient(r,i,N)
     end
-    U[N_iter+1] += PotentialEnergy(r,N)
-    potential = 
-    println("Iteration $N_iter Total Grad ",norm(tot_grad)," x: ",abs(tot_grad[1])," y: ",abs(tot_grad[2]), " z: ", abs(tot_grad[3]))
-    #println("Iter $N_iter - ",PotentialEnergy(r,N))
-    # if abs(U[N_iter+1]-U[N_iter]) ≤ max_tol
-    #      break
-    # end
-    if norm(tot_grad) ≤ max_tol
+    U[2] = PotentialEnergy(r,N)
+    println("Iter $N_iter) U: ",PotentialEnergy(r,N)," x: ",abs(tot_grad[1])," y: ",abs(tot_grad[2]), " z: ", abs(tot_grad[3]))
+    if abs(U[2]-U[1]) ≤ max_tol
         break
-   end
-
+    end
     N_iter += 1
+    U[1] = U[2]
 end
 
-U = PotentialEnergy(r,N)
-
-
+#---Particles position---#
 x = r[:,1]
 y = r[:,2]
 z = r[:,3]
-PlotSphere(x,y,z,"Gradient")
+
+#---Output---#
 println("-----Gradient Descent-----")
-println("Potential: $U")
-println(r[1,:])
-println(r[2,:])
+println("Converged at iter $N_iter with U = ",U[2])
+
+#---Plot---#
+PlotSphere(x,y,z,"Gradient")
+
+
 
 
 
