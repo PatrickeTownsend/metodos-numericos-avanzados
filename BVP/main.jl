1c81004b5003893e235f92e2448a0312516787ee
PatrickeTownsend <p.townsend.2019@alumnos.urjc.es>
2024-01-22 01:22:47 +0100

    updated

--- BVP/main.jl
+++ BVP/main.jl
@@ -1,36 +1,67 @@
 using LinearAlgebra
 using Plots
+
+# Set problem parameters:
+k  = 1.     # k: square of oscillator's frequency
+y0 = 1.     # y0: initial value for y(t0)
+
+
+# Timespan
 t0 = 0.
 tf = 10.
+
+
+# Time discretization
 N = 100
-k = 1.
-h = (tf-t0)/(N+1)
+h = @. (tf-t0) / N
 
-A = zeros(Float64, N, N)
-B = zeros(Float64, N, 1)
-B[1] = -h - k*h^2
 
-#bands
-for i=2:(N-1)
-    A[i,i-1] = 1
-    A[i,i] = -2. + k*h^2
-    A[i,i+1] = 1.
-end
+# Initialize matrix A and vector b
+A = @. ones(Float64, N, N) * 0.
+b = @. ones(Float64, N, 1) * 0.
 
-# i=1
-A[1,1] =  k*h^2 - 2*h
-A[1,2] = 1.
 
-# i = N
-A[N,N-1] = -h
-A[N,N] = h
+# Fill in vector b
+b[1] = - y0
 
-y = A\B
-t = zeros(Float64,N,1)
-for i in eachindex(t)
+
+# Fill in matrix A:
+
+    # Diagonal bands
+    for i in 2:(N-1)
+        A[i,i-1] =  1.
+        A[i,i]   = @. -2 + k*h^2
+        A[i,i+1] =  1.
+    end
+
+    # i = 1 (Impose boundary value at t1 = t0 + h)
+    A[1,1] = @. k*h^2 - 2
+    A[1,2] = 1.
+
+    # i = N (Impose boundary value at tf)
+    A[N,N-1] = -h
+    A[N,N]   =  h
+
+
+# Solve Linear System: Get solution y(t) at discrete instants of time
+y = A\b
+
+
+# Add value at t0 to the solution
+y = [y0; y]
+
+
+# Get vector for equi-spaced, discretized time values
+t = ones(Float64, N, 1)
+for i in 1:N
     t[i] = i .* h
 end
+t = [t0; t]
 
-plot(t,y)
 
+# Plot solution
+p = plot(t, y)
+display(p)
+plot!(t, y, seriestype=:scatter, ms=2)
 
+println("Done!")
